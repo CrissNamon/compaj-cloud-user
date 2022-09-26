1683862a2f83dc386813513fef1bcd5c5c26825c
CrissNamon <deus.ex.00@mail.ru>
2022-09-26 15:18:07 +0300

    Updated bootstrap. Fixed logout redirect

--- src/main/resources/static/js/watcher.js
+++ src/main/resources/static/js/watcher.js
@@ -3,7 +3,7 @@ window.onload = function () {
   source.addEventListener('message', function (e) {
     if (e.data) {
       const payload = JSON.parse(e.data);
-      $(".container").append('<p>' + payload.action + '</p>')
+      $("#data").append("<p>" + payload.action.replace(/\n/g, "<br>") + "</p>");
     }
   });
   source.stream();
